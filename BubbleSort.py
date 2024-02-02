dc8f7176a12b4aeb1f63ad23d8313817224cba7a
Milrael <elraden94@gmail.com>
2024-02-02 14:35:17 +0300

    Алгоритм пузырьковой сортировки

---
+++ BubbleSort.py
@@ -0,0 +1,35 @@
+#Вариант 1
+def bubble_sort(arr):
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    n = len(arr)
+    swapped = True
+
+    x = -1
+    while swapped:
+        swapped = False
+        x = x + 1
+        for i in range(1, n - x):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+
+#Вариант 2
+def bubbleSort(arr):
+    swapped = False
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if swapped:
+            swapped = False
+        else: 
+            break
+    return arr
+
+
+
+random_list = [5, 22, 1, 8 ,7, 30]
+print(bubbleSort(random_list))
